bdb7986eb09596008601348b995e5dd331edf328
Juliana Petravicius <135447143+julianascpetravicius@users.noreply.github.com>
2024-06-26 14:51:57 +0100

    Mentoria e Curso

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("descrição do curso java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso Js");
+        curso2.setDescricao("descrição do curso js");
+        curso2.setCargaHoraria(4);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("mentoria de java");
+        mentoria.setDescricao("deescrição mentoria java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso2);
+        System.out.println(curso1);
+        System.out.println(mentoria );
+
+
     }
 }
